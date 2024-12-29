a8d2aa152d3ad602733eadbf4d293d45a34bbe12
dharaseeme <154053132+dharaseeme@users.noreply.github.com>
2024-12-29 21:30:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Ԁϲгคz̼͖̺̠̰͇̙̓͛ͮͩͦ̎ͦ̑ͅ⑂ϲคԀєt̘̟̼̉̈́͐͋͌̊',
   botName: "Z̸͟͞E̸͟͞R̸͟͞O̸͟͞C̸͟͞O̸͟͞O̸͟͞L̸͟͞",
   botOwner: 'Ԁϲгคz̼͖̺̠̰͇̙̓͛ͮͩͦ̎ͦ̑ͅ⑂ϲคԀєt̘̟̼̉̈́͐͋͌̊', // Your name
-  ownerNumber: 2348030664873', //Your number
+  ownerNumber: '2348030664873', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
