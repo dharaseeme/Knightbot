3c295a9ac59386de763be74285b0cd2b75f0a376
dharaseeme <154053132+dharaseeme@users.noreply.github.com>
2024-12-29 21:29:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Ԁϲгคz̼͖̺̠̰͇̙̓͛ͮͩͦ̎ͦ̑ͅ⑂ϲคԀєt̘̟̼̉̈́͐͋͌̊',
+  author: 'Ԁϲгคz̼͖̺̠̰͇̙̓͛ͮͩͦ̎ͦ̑ͅ⑂ϲคԀєt̘̟̼̉̈́͐͋͌̊',
+  botName: "Z̸͟͞E̸͟͞R̸͟͞O̸͟͞C̸͟͞O̸͟͞O̸͟͞L̸͟͞",
+  botOwner: 'Ԁϲгคz̼͖̺̠̰͇̙̓͛ͮͩͦ̎ͦ̑ͅ⑂ϲคԀєt̘̟̼̉̈́͐͋͌̊', // Your name
+  ownerNumber: 2348030664873', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
